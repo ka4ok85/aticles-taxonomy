f69d93aec3ed48717ac8a0cec75566893e3cd937
ka4ok85 <makovetsky.evgeny@gmail.com>
2018-01-15 14:26:34 -0500

    init

---
+++ src/main/java/com/example/aticlestaxonomy/AticlesTaxonomyApplication.java
@@ -0,0 +1,12 @@
+package com.example.aticlestaxonomy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AticlesTaxonomyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AticlesTaxonomyApplication.class, args);
+	}
+}

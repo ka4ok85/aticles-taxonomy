4c1776edaa7770aecf492fe12662219158a0afbf
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-01-19 19:01:51 -0500

    added Category entity

---
+++ src/main/java/com/example/aticlestaxonomy/entities/Category.java
@@ -0,0 +1,42 @@
+package com.example.aticlestaxonomy.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(unique = true)
+	private String category;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	@Override
+	public String toString() {
+		return "Category [id=" + id + ", category=" + category + "]";
+	}
+
+}

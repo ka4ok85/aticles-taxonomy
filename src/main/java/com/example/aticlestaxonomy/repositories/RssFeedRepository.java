1b78178ab4c14d6d74b45141ae12d9e0e7bf1f68
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-01-20 20:39:32 -0500

    added RssFeedRepository repository

---
+++ src/main/java/com/example/aticlestaxonomy/repositories/RssFeedRepository.java
@@ -0,0 +1,9 @@
+package com.example.aticlestaxonomy.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.aticlestaxonomy.entities.RssFeed;
+
+public interface RssFeedRepository extends CrudRepository<RssFeed, Long> {
+
+}

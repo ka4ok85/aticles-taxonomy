fa2e61a5d729a518158eaf8de0e444ecccb4041e
ka4ok85 <makovetsky.evgeny@gmail.com>
2018-01-22 15:58:26 -0500

    added ArticleJoinCategory entity

---
+++ src/main/java/com/example/aticlestaxonomy/entities/ArticleJoinCategory.java
@@ -0,0 +1,69 @@
+package com.example.aticlestaxonomy.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name="article_join_category")
+public class ArticleJoinCategory {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "article_id", unique = false, nullable = false)
+	@NotNull
+	private Long articleId;
+	
+	@Column(name = "category_id", unique = false, nullable = false)
+	@NotNull
+	private Long categoryId;
+
+	@Column(name = "weight", unique = false, nullable = false)
+	@NotNull
+	private Long weight;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getArticleId() {
+		return articleId;
+	}
+
+	public void setArticleId(Long articleId) {
+		this.articleId = articleId;
+	}
+
+	public Long getCategoryId() {
+		return categoryId;
+	}
+
+	public void setCategoryId(Long categoryId) {
+		this.categoryId = categoryId;
+	}
+
+	public Long getWeight() {
+		return weight;
+	}
+
+	public void setWeight(Long weight) {
+		this.weight = weight;
+	}
+
+	@Override
+	public String toString() {
+		return "ArticleJoinCategory [id=" + id + ", articleId=" + articleId + ", categoryId=" + categoryId + ", weight="
+				+ weight + "]";
+	}
+	
+}

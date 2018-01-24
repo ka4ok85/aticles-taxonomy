c7ccb2db1f65e7bbaad65f0d88e72015d92ca503
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-01-24 14:48:02 -0500

    added WebResourceService interface

---
+++ src/main/java/com/example/aticlestaxonomy/services/WebResourceService.java
@@ -0,0 +1,9 @@
+package com.example.aticlestaxonomy.services;
+
+import java.util.List;
+
+import com.example.aticlestaxonomy.entities.WebResource;
+
+public interface WebResourceService {
+	public List<WebResource> getWebResourcesAvaialbleForProcess();
+}

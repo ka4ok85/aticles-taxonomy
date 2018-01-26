49bdc31ec07dd484cbeb4da17ff3ef531642ea09
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-01-25 21:14:57 -0500

    added findRssFeedsReadyForProcessing() method to the RssFeedRepository

--- src/main/java/com/example/aticlestaxonomy/repositories/RssFeedRepository.java
+++ src/main/java/com/example/aticlestaxonomy/repositories/RssFeedRepository.java
@@ -1,9 +1,14 @@
 package com.example.aticlestaxonomy.repositories;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.example.aticlestaxonomy.entities.RssFeed;
 
 public interface RssFeedRepository extends CrudRepository<RssFeed, Long> {
 
+	@Query(value = "SELECT * FROM rss_feeds WHERE `status`='enabled' and DATE_ADD(last_fetch_datetime, INTERVAL fetch_window second) <= NOW()", nativeQuery = true)
+	public List<RssFeed> findRssFeedsReadyForProcessing();
 }

488219470190f0d1f3b1d9c479c24957e04bf4ae
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-01-27 18:31:17 -0500

    added AbstractRssFeedReader service

---
+++ src/main/java/com/example/aticlestaxonomy/services/AbstractRssFeedReader.java
@@ -0,0 +1,23 @@
+package com.example.aticlestaxonomy.services;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.aticlestaxonomy.dto.Article;
+
+public abstract class AbstractRssFeedReader {
+
+	protected String url;
+	protected LocalDateTime lastFetchDate;
+	protected List<Article> articles = new ArrayList<Article>();
+
+	public AbstractRssFeedReader(String url, LocalDateTime lastFetchDate) {
+		super();
+		this.url = url;
+		this.lastFetchDate = lastFetchDate;
+	}
+
+	abstract int readRssFeed();
+
+}

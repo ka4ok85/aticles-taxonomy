ef3d8a4f0c17a749866dca89cb8fcc2bf67a4355
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-01-26 20:01:31 -0500

    added Article DTO

---
+++ src/main/java/com/example/aticlestaxonomy/dto/Article.java
@@ -0,0 +1,39 @@
+package com.example.aticlestaxonomy.dto;
+
+import java.time.LocalDateTime;
+
+public class Article {
+	private String title;
+	private LocalDateTime pubDate;
+	private String link;
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public LocalDateTime getPubDate() {
+		return pubDate;
+	}
+
+	public void setPubDate(LocalDateTime pubDate) {
+		this.pubDate = pubDate;
+	}
+
+	public String getLink() {
+		return link;
+	}
+
+	public void setLink(String link) {
+		this.link = link;
+	}
+
+	@Override
+	public String toString() {
+		return "Article [title=" + title + ", pubDate=" + pubDate + ", link=" + link + "]";
+	}
+
+}

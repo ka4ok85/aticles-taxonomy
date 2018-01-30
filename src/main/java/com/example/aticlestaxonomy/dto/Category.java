b73c33ca60e4b2bdb6933a201d59972596f9cfb5
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-01-29 20:47:38 -0500

    added Category DTO

---
+++ src/main/java/com/example/aticlestaxonomy/dto/Category.java
@@ -0,0 +1,19 @@
+package com.example.aticlestaxonomy.dto;
+
+public class Category {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Category [name=" + name + "]";
+	}
+
+}

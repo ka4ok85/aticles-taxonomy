8452c377dc1930981630e90697553dbd197dc884
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-01-30 14:43:34 -0500

    added ArticleService implementation

---
+++ src/main/java/com/example/aticlestaxonomy/services/ArticleServiceImplementation.java
@@ -0,0 +1,29 @@
+package com.example.aticlestaxonomy.services;
+
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.aticlestaxonomy.entities.Article;
+import com.example.aticlestaxonomy.repositories.ArticleRepository;
+
+public class ArticleServiceImplementation implements ArticleService {
+
+	@Autowired
+	private ArticleRepository articleRepository;
+	
+	@Override
+	public Article saveArticle(com.example.aticlestaxonomy.dto.Article article, Long rssFeedId) {
+		Article articleEntity = new Article();
+		articleEntity.setTitle(article.getTitle());
+		articleEntity.setUrl(article.getLink());
+		articleEntity.setCreatedDatetime(article.getPubDate());
+		articleEntity.setFetchedDatetime(LocalDateTime.now());
+		articleEntity.setIsCategorySet(0L);
+		articleEntity.setRssFeedId(rssFeedId);
+		
+		return articleRepository.save(articleEntity);
+		//return null;
+	}
+
+}

288ba8cec003894cfe2e7c31807df463ddd326b2
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-02-01 20:53:35 -0500

    custom feed processing using command line runner

--- src/main/java/com/example/aticlestaxonomy/CustomCommandLineRunner.java
+++ src/main/java/com/example/aticlestaxonomy/CustomCommandLineRunner.java
@@ -1,13 +1,21 @@
 package com.example.aticlestaxonomy;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Future;
+import java.util.stream.Collector;
+import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
 import com.example.aticlestaxonomy.entities.Article;
+import com.example.aticlestaxonomy.entities.RssFeed;
 import com.example.aticlestaxonomy.repositories.ArticleRepository;
+import com.example.aticlestaxonomy.repositories.RssFeedRepository;
+import com.example.aticlestaxonomy.services.RssFeedService;
 
 @Configuration
 public class CustomCommandLineRunner implements CommandLineRunner {
@@ -15,18 +23,55 @@ public class CustomCommandLineRunner implements CommandLineRunner {
 	@Autowired
 	private ArticleRepository articleRepository;
 	
+	@Autowired
+	private RssFeedRepository rssFeedRepository;
+	
+	@Autowired
+	private RssFeedService rssFeedService;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
 		
 		System.out.println("Starting CustomCommandLineRunner");
 		
+		/*
+		List<RssFeed> y = rssFeedRepository.findRssFeedsReadyForProcessing();
+		for (RssFeed rssFeed : y) {
+			System.out.println(rssFeed);
+		}
+		*/
 		
-		Optional<Article> optionalArticle = articleRepository.findById(1L);
-		Article article = optionalArticle.orElse(null);
 		
-		System.out.println(article);
+		List<RssFeed> rssFeeds = rssFeedService.getRssFeedsAvaialbleForProcess();
+		//System.out.println(rssFeeds);
 		
+		//CompletableFuture<Integer> future = new CompletableFuture<Integer>();
+		
+		
+		/*
+		Collector<Stream<Integer>, Integer, Integer> y;
+		Stream<Integer> t = rssFeeds.stream().map(rssFeedService::processFeed).collect(y);
+		*/
+		
+		
+		for (RssFeed rssFeed : rssFeeds) {
+			System.out.println(rssFeed);
+			rssFeedService.processFeed(rssFeed);
+			rssFeedService.setLastFetchDatetimeToNow(rssFeed);
+			/*
+			CompletableFuture<Integer> getUsersDetail(RssFeed r) {
+				return CompletableFuture.thenApplyAsync((RssFeed r) -> {
+					rssFeedService.processFeed(r);
+				});	
+			};
+			*/			
+			
+		}
+
+		
+		System.out.println("Ended CustomCommandLineRunner");
+
 		
 		
 	}

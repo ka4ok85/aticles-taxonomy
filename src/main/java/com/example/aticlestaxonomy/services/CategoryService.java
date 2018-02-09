bde7b45bdbb44080f0a2bece93554938baaf1c4d
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-02-08 20:02:47 -0500

    added getArticleCategories() Watson implementation

--- src/main/java/com/example/aticlestaxonomy/services/CategoryService.java
+++ src/main/java/com/example/aticlestaxonomy/services/CategoryService.java
@@ -1,8 +1,7 @@
 package com.example.aticlestaxonomy.services;
 
-import java.util.List;
 import java.util.Map;
 
 public interface CategoryService {
-	public List<Map<String, Double>> getArticleCategories(String url);
+	public Map<String, Double> getArticleCategories(String url);
 }
--- src/main/java/com/example/aticlestaxonomy/services/CategoryServiceWatsonImplementation.java
+++ src/main/java/com/example/aticlestaxonomy/services/CategoryServiceWatsonImplementation.java
@@ -1,14 +1,38 @@
 package com.example.aticlestaxonomy.services;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.ibm.watson.developer_cloud.natural_language_understanding.v1.NaturalLanguageUnderstanding;
+import com.ibm.watson.developer_cloud.natural_language_understanding.v1.model.AnalysisResults;
+import com.ibm.watson.developer_cloud.natural_language_understanding.v1.model.AnalyzeOptions;
+import com.ibm.watson.developer_cloud.natural_language_understanding.v1.model.CategoriesOptions;
+import com.ibm.watson.developer_cloud.natural_language_understanding.v1.model.CategoriesResult;
+import com.ibm.watson.developer_cloud.natural_language_understanding.v1.model.Features;
+
 public class CategoryServiceWatsonImplementation implements CategoryService {
+	@Autowired
+	protected NaturalLanguageUnderstanding service;
 
 	@Override
-	public List<Map<String, Double>> getArticleCategories(String url) {
-		// TODO Auto-generated method stub
-		return null;
+	public Map<String, Double> getArticleCategories(String url) {
+		Map<String, Double> results = new HashMap<String, Double>();
+
+		CategoriesOptions categories = new CategoriesOptions();
+		Features features = new Features.Builder().categories(categories).build();
+		AnalyzeOptions parameters = new AnalyzeOptions.Builder().url(url).features(features).build();
+		AnalysisResults analysisResults = service.analyze(parameters).execute();
+
+		List<CategoriesResult> categoriesResultList = analysisResults.getCategories();
+		for (CategoriesResult categoriesResult : categoriesResultList) {
+			results.put(categoriesResult.getLabel(), categoriesResult.getScore());
+		}
+
+		return results;
 	}
 
 }

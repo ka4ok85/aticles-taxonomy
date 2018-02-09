7b349e870cfba1e7ccd6f44172d79c960cf9ae32
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-02-08 19:55:35 -0500

    added CategoryServiceWatsonImplementation

---
+++ src/main/java/com/example/aticlestaxonomy/services/CategoryServiceWatsonImplementation.java
@@ -0,0 +1,14 @@
+package com.example.aticlestaxonomy.services;
+
+import java.util.List;
+import java.util.Map;
+
+public class CategoryServiceWatsonImplementation implements CategoryService {
+
+	@Override
+	public List<Map<String, Double>> getArticleCategories(String url) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

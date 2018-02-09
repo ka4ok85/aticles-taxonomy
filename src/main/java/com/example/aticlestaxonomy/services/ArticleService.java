c28a4484acf6c7dbd9064444804f54f11552b54f
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-02-08 20:06:33 -0500

    added getArticlesAvaialbleForAnalysis() to the interface

--- src/main/java/com/example/aticlestaxonomy/services/ArticleService.java
+++ src/main/java/com/example/aticlestaxonomy/services/ArticleService.java
@@ -1,5 +1,8 @@
 package com.example.aticlestaxonomy.services;
 
+import java.util.List;
+
 public interface ArticleService {
 	public com.example.aticlestaxonomy.entities.Article saveArticle(com.example.aticlestaxonomy.dto.Article article, Long rssFeedId);
+	public List<com.example.aticlestaxonomy.entities.Article> getArticlesAvaialbleForAnalysis();
 }

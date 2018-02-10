4d2caf7fbd873205e33bba3677df14da51270e4d
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-02-09 20:08:16 -0500

    added findByIsCategorySet() to the repo interface

--- src/main/java/com/example/aticlestaxonomy/repositories/ArticleRepository.java
+++ src/main/java/com/example/aticlestaxonomy/repositories/ArticleRepository.java
@@ -1,9 +1,11 @@
 package com.example.aticlestaxonomy.repositories;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.example.aticlestaxonomy.entities.Article;
 
 public interface ArticleRepository extends CrudRepository<Article, Long> {
-
+    List<Article> findByIsCategorySet(Long isCategorySet);
 }

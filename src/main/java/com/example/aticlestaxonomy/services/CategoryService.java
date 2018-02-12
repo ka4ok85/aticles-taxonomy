da4aa7d9572e2baec46476de4eb137101970d5d5
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-02-12 13:47:50 -0500

    added findByCategory() to category interface

--- src/main/java/com/example/aticlestaxonomy/services/CategoryService.java
+++ src/main/java/com/example/aticlestaxonomy/services/CategoryService.java
@@ -2,6 +2,9 @@ package com.example.aticlestaxonomy.services;
 
 import java.util.Map;
 
+import com.example.aticlestaxonomy.entities.Category;
+
 public interface CategoryService {
 	public Map<String, Double> getArticleCategories(String url);
+	public Category findByCategory(String category);
 }

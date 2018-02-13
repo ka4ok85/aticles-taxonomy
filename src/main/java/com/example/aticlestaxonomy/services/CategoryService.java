e2b75ebb7f972f3907ad78bf84665a06cdfee94c
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-02-13 16:27:54 -0500

    updated getArticleCategories() to use Completable Future

--- src/main/java/com/example/aticlestaxonomy/services/CategoryService.java
+++ src/main/java/com/example/aticlestaxonomy/services/CategoryService.java
@@ -1,10 +1,11 @@
 package com.example.aticlestaxonomy.services;
 
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 
 import com.example.aticlestaxonomy.entities.Category;
 
 public interface CategoryService {
-	public Map<String, Double> getArticleCategories(String url);
+	public CompletableFuture<Map<String, Double>> getArticleCategories(String url);
 	public Category findByCategory(String category);
 }

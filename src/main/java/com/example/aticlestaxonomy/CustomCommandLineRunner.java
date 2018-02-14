ad167926ee26fe4d6791dfff8cba2667f8ffb9af
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-02-14 14:11:28 -0500

    added stream processing logic for articles

--- src/main/java/com/example/aticlestaxonomy/CustomCommandLineRunner.java
+++ src/main/java/com/example/aticlestaxonomy/CustomCommandLineRunner.java
@@ -1,6 +1,11 @@
 package com.example.aticlestaxonomy;
 
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.slf4j.Logger;
@@ -38,11 +43,116 @@ public class CustomCommandLineRunner implements CommandLineRunner {
 	@Transactional
 	public void run(String... args) throws Exception {
 		log.info("Starting CustomCommandLineRunner");
+//getArticleCategories
+		  List<Article> articles = articleService.getArticlesAvaialbleForAnalysis();
+		  if (articles.size() == 0) { 
+			  log.info("All articles already processed."); 
+			  log.info("Ended CustomCommandLineRunner"); 
+			  return ; 
+		  }
+		  System.out.println(articles.size());
+/*
+		  List<CompletableFuture<Map<String,Double>>> articlesFutures = 
+				  articles.stream()
+				  			.map(article -> categoryService.getArticleCategories2(article.getUrl()))
+				  			.collect(Collectors.toList()
+		  );
+*/
+		  /*
+		  List<CompletableFuture<Article>> articlesFutures2 = 
+				  articles.stream()
+				  			.map((Article article) -> {
+				  				CompletableFuture<Map<String, Double>> categories = categoryService.getArticleCategories2(article.getUrl())	;
+				  				categories.thenApply(
+				  					x -> {
+				  						CompletableFuture<Article> d = articleService.setCatgoriesForArticle(article, categories);
+				  						return d;
+				  					}
+				  				)
+				  				//CompletableFuture<Article> updatedArticle = articleService.setCatgoriesForArticle(article, categories);
+				  				//return null;
+				  				//return updatedArticle;
+				  			}
+				  			)
+				  			//.map(d -> articleService.setCatgoriesForArticle(article, categories))
+				  			.collect(Collectors.toList()
+		  );
+		  */
+		  
+//		  List<CompletableFuture<Map<String,Double>>> articlesFutures = 
+				Function<Article, CompletableFuture<Article>> mapper = new Function<Article, CompletableFuture<Article>>() {
 
+					@Override
+					public CompletableFuture<Article> apply(Article article) {
+						CompletableFuture<Map<String, Double>> categories = categoryService.getArticleCategories(article.getUrl())	;
+
+						 // raw
+						Function<Map<String, Double>, Article> fn = new Function<Map<String,Double>, Article>() {
+
+							//@Override
+							public CompletableFuture<Article> apply(CompletableFuture<Map<String, Double>> t) {
+								//CompletableFuture<Article> d = articleService.setCatgoriesForArticle(article, t);
+		  						return articleService.setCatgoriesForArticle(article, t);
+								//return null;
+							}
+// public CompletableFuture<Article> setCatgoriesForArticle(Article article, CompletableFuture<Map<String,Double>> categories);
+
+							@Override
+							public Article apply(Map<String, Double> t) {
+								return articleService.setCatgoriesForArticle2(article, t);
+								//return null;
+							}
+
+							
+						};
+
+						/*
+						Function<CompletableFuture<Map<String, Double>>, CompletableFuture<Article>> fn = new Function<CompletableFuture<Map<String,Double>>, CompletableFuture<Article>>() {
+
+							//@Override
+							public CompletableFuture<Article> apply(CompletableFuture<Map<String, Double>> t) {
+								//CompletableFuture<Article> d = articleService.setCatgoriesForArticle(article, t);
+		  						return articleService.setCatgoriesForArticle(article, t);
+								//return null;
+							}
+// public CompletableFuture<Article> setCatgoriesForArticle(Article article, CompletableFuture<Map<String,Double>> categories);
+						
+						};
+//						CompletableFuture<Article> z = categories.thenApply(fn);
+						CompletableFuture<Article> z = categories.thenApply(
+								v -> { return new CompletableFuture<Article>();}
+						);
+						*/
+						CompletableFuture<Article> z = categories.thenApply(fn);
+						return   z;
+						/*
+						CompletableFuture<Article> z = categories.thenApply(
+			  					x -> {
+			  						CompletableFuture<Article> d = articleService.setCatgoriesForArticle(article, categories);
+			  						return d;
+			  					}
+			  				);
+						
+						return   z;
+						*/
+					}
+					
+				};
+
+				List<CompletableFuture<Article>> articlesFutures2 = articles.stream().map(mapper)
+				  			.collect(Collectors.toList());
+		  
+		  //CompletableFuture<Void> allFutures = CompletableFuture.allOf(articlesFutures.toArray(new CompletableFuture[articlesFutures.size()]) );
+		  CompletableFuture<Void> allFutures2 = CompletableFuture.allOf(articlesFutures2.toArray(new CompletableFuture[articlesFutures2.size()]) );
+		  
+		  allFutures2.get();
+/*
 		try (Stream<Article> articles = articleService.getArticlesAvaialbleForAnalysis()) {
 
 			articles.forEach(
 					article -> {
+						log.info("Starting processing article ID={}, URL={}", article.getId(), article.getUrl());
+
 						Set<ArticleJoinCategory> articleJoinCategorySet = article.getArticleJoinCategorySet();
 						categoryService.getArticleCategories(article.getUrl())
 								.forEach((String category, Double value) -> {
@@ -56,10 +166,14 @@ public class CustomCommandLineRunner implements CommandLineRunner {
 						article.setIsCategorySet(1L);
 						articleRepository.save(article);
 
+						log.info("End processing article ID={}, URL={}", article.getId(), article.getUrl());
 					}
 			);
+
 		}
-/*
+*/
+		
+		/*
 		  List<RssFeed> rssFeeds =
 		  rssFeedService.getRssFeedsAvaialbleForProcess(); if (rssFeeds.size()
 		  == 0) { log.info("All feeds already processed."); log.info(

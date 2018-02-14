6930b402340f11d18520c8b76b623a0b877f460c
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-02-14 14:07:50 -0500

    added setCatgoriesForArticle()

--- src/main/java/com/example/aticlestaxonomy/services/ArticleService.java
+++ src/main/java/com/example/aticlestaxonomy/services/ArticleService.java
@@ -1,10 +1,12 @@
 package com.example.aticlestaxonomy.services;
 
-import java.util.stream.Stream;
+import java.util.List;
+import java.util.Map;
 
 import com.example.aticlestaxonomy.entities.Article;
 
 public interface ArticleService {
 	public com.example.aticlestaxonomy.entities.Article addArticle(com.example.aticlestaxonomy.dto.Article article, Long rssFeedId);
-	public Stream<Article> getArticlesAvaialbleForAnalysis();
+	public List<Article> getArticlesAvaialbleForAnalysis();
+	public Article setCatgoriesForArticle(Article article, Map<String, Double> t);
 }

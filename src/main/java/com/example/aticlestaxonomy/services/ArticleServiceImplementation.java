bfaab1a48a47419374284c0fc2620a205dabfa8c
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-02-14 14:10:01 -0500

    added setCatgoriesForArticle() implementation

--- src/main/java/com/example/aticlestaxonomy/services/ArticleServiceImplementation.java
+++ src/main/java/com/example/aticlestaxonomy/services/ArticleServiceImplementation.java
@@ -1,17 +1,23 @@
 package com.example.aticlestaxonomy.services;
 
 import java.time.LocalDateTime;
-import java.util.stream.Stream;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.example.aticlestaxonomy.entities.Article;
+import com.example.aticlestaxonomy.entities.ArticleJoinCategory;
 import com.example.aticlestaxonomy.repositories.ArticleRepository;
 
 public class ArticleServiceImplementation implements ArticleService {
 
 	@Autowired
 	private ArticleRepository articleRepository;
+
+	@Autowired
+	private CategoryService categoryService;
 
 	@Override
 	public Article addArticle(com.example.aticlestaxonomy.dto.Article article, Long rssFeedId) {
@@ -27,8 +33,23 @@ public class ArticleServiceImplementation implements ArticleService {
 	}
 
 	@Override
-	public Stream<Article> getArticlesAvaialbleForAnalysis() {
+	public List<Article> getArticlesAvaialbleForAnalysis() {
 		return articleRepository.findByIsCategorySet(0L);
+	}
+
+	public Article setCatgoriesForArticle(final Article article, Map<String, Double> categories) {
+		Set<ArticleJoinCategory> articleJoinCategorySet = article.getArticleJoinCategorySet();
+		categories.forEach((String category, Double value) -> {
+			ArticleJoinCategory articleJoinCategory = new ArticleJoinCategory();
+			articleJoinCategory.setArticleId(article.getId());
+			articleJoinCategory.setCategoryId(categoryService.findByCategory(category).getId());
+			articleJoinCategory.setWeight(value.floatValue());
+			articleJoinCategorySet.add(articleJoinCategory);
+		});
+		article.setArticleJoinCategorySet(articleJoinCategorySet);
+		article.setIsCategorySet(1L);
+
+		return articleRepository.save(article);
 	}
 
 }

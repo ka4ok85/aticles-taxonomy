203c8df8aff7f449d10dcc8c4d9edbc052efb75a
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-02-15 16:57:21 -0500

    added Articles Controller

---
+++ src/main/java/com/example/aticlestaxonomy/rest/v1/ArticleController.java
@@ -0,0 +1,29 @@
+package com.example.aticlestaxonomy.rest.v1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.aticlestaxonomy.dto.Article;
+import com.example.aticlestaxonomy.dto.Category;
+
+@RestController("ArticleControllerV1")
+@RequestMapping("/v1/")
+public class ArticleController {
+
+	@RequestMapping(value = "/articles", method = RequestMethod.GET)
+	public ResponseEntity<List<Article>> getProducts(@RequestParam String categories) {
+
+		List<Article> articles = new ArrayList<Article>();
+		System.out.println(categories);
+
+		return new ResponseEntity<List<Article>>(articles, HttpStatus.OK);
+	}
+	
+}

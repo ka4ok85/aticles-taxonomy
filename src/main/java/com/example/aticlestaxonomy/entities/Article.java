3f0c284af103453558dcd4d4b0e7dfd8cf2d9f92
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-02-16 17:04:19 -0500

    added hashCode() and equals() to Article entity

--- src/main/java/com/example/aticlestaxonomy/entities/Article.java
+++ src/main/java/com/example/aticlestaxonomy/entities/Article.java
@@ -116,6 +116,73 @@ public class Article {
 	}
 
 	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((articleJoinCategorySet == null) ? 0 : articleJoinCategorySet.hashCode());
+		result = prime * result + ((createdDatetime == null) ? 0 : createdDatetime.hashCode());
+		result = prime * result + ((fetchedDatetime == null) ? 0 : fetchedDatetime.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((isCategorySet == null) ? 0 : isCategorySet.hashCode());
+		result = prime * result + ((rssFeedId == null) ? 0 : rssFeedId.hashCode());
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		result = prime * result + ((url == null) ? 0 : url.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Article other = (Article) obj;
+		if (articleJoinCategorySet == null) {
+			if (other.articleJoinCategorySet != null)
+				return false;
+		} else if (!articleJoinCategorySet.equals(other.articleJoinCategorySet))
+			return false;
+		if (createdDatetime == null) {
+			if (other.createdDatetime != null)
+				return false;
+		} else if (!createdDatetime.equals(other.createdDatetime))
+			return false;
+		if (fetchedDatetime == null) {
+			if (other.fetchedDatetime != null)
+				return false;
+		} else if (!fetchedDatetime.equals(other.fetchedDatetime))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (isCategorySet == null) {
+			if (other.isCategorySet != null)
+				return false;
+		} else if (!isCategorySet.equals(other.isCategorySet))
+			return false;
+		if (rssFeedId == null) {
+			if (other.rssFeedId != null)
+				return false;
+		} else if (!rssFeedId.equals(other.rssFeedId))
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		if (url == null) {
+			if (other.url != null)
+				return false;
+		} else if (!url.equals(other.url))
+			return false;
+		return true;
+	}
+
+	@Override
 	public String toString() {
 		return "Article [id=" + id + ", url=" + url + ", title=" + title + ", createdDatetime=" + createdDatetime
 				+ ", fetchedDatetime=" + fetchedDatetime + ", rssFeedId=" + rssFeedId + ", isCategorySet="

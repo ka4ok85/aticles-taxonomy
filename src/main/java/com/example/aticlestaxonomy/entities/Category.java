5909a9ee6f7ad0c4409731422b07b3554eea8d32
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-02-16 17:05:01 -0500

    added hashCode() and equals() to Category entity

--- src/main/java/com/example/aticlestaxonomy/entities/Category.java
+++ src/main/java/com/example/aticlestaxonomy/entities/Category.java
@@ -35,6 +35,37 @@ public class Category {
 	}
 
 	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((category == null) ? 0 : category.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Category other = (Category) obj;
+		if (category == null) {
+			if (other.category != null)
+				return false;
+		} else if (!category.equals(other.category))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
 	public String toString() {
 		return "Category [id=" + id + ", category=" + category + "]";
 	}

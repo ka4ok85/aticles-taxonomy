9cbadfd37a7a742c8db8ef6e77b204f560c0752a
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-02-17 19:01:34 -0500

    added check for category existence

--- src/main/java/com/example/aticlestaxonomy/rest/v1/ArticleController.java
+++ src/main/java/com/example/aticlestaxonomy/rest/v1/ArticleController.java
@@ -5,31 +5,50 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.aticlestaxonomy.dto.Article;
 import com.example.aticlestaxonomy.dto.Category;
 import com.example.aticlestaxonomy.dto.ErrorInfo;
+import com.example.aticlestaxonomy.services.CategoryService;
 
 @RestController("ArticleControllerV1")
 @RequestMapping("/v1/")
 public class ArticleController {
-    @RequestMapping(value = "/articles/search", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
-	public Object /*ResponseEntity<?> /*ResponseEntity<List<Article>>*/ getProducts(@RequestBody List<Category> categories) {
+
+	@Autowired
+	private CategoryService categoryService;
+
+	@RequestMapping(value = "/articles/search", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
+	public ResponseEntity<?> getArticles(@RequestBody List<Category> categories) {
 
 		List<Article> articles = new ArrayList<Article>();
-		if (categories.size() < 1 ) {
-			ErrorInfo errorInfo = new ErrorInfo("Bad Request", HttpServletResponse.SC_BAD_REQUEST, "No categories were specified in Request", this.getClass().getName());
+		if (categories.size() < 1) {
+			ErrorInfo errorInfo = new ErrorInfo("Bad Request", HttpServletResponse.SC_BAD_REQUEST,
+					"No categories were specified in Request", this.getClass().getName());
+
 			return new ResponseEntity<ErrorInfo>(errorInfo, HttpStatus.BAD_REQUEST);
+		}
+
+		for (Category category : categories) {
+			if (null == categoryService.findByCategory(category.getName())) {
+				ErrorInfo errorInfo = new ErrorInfo("Bad Request", HttpServletResponse.SC_BAD_REQUEST,
+						"Category " + category.getName() + " does not exist", this.getClass().getName());
+
+				return new ResponseEntity<ErrorInfo>(errorInfo, HttpStatus.BAD_REQUEST);
+			} else {
+				System.out.println(category.getName());
+			}
+
 		}
 
 		return new ResponseEntity<List<Article>>(articles, HttpStatus.OK);
 	}
-	
+
 }

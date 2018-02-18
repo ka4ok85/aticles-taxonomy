94a71e081441aed087e486d43f3a207857b7b571
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-02-18 16:56:55 -0500

    added getArticlesByCategories()

--- src/main/java/com/example/aticlestaxonomy/services/ArticleService.java
+++ src/main/java/com/example/aticlestaxonomy/services/ArticleService.java
@@ -3,10 +3,12 @@ package com.example.aticlestaxonomy.services;
 import java.util.List;
 import java.util.Map;
 
+import com.example.aticlestaxonomy.dto.Category;
 import com.example.aticlestaxonomy.entities.Article;
 
 public interface ArticleService {
 	public com.example.aticlestaxonomy.entities.Article addArticle(com.example.aticlestaxonomy.dto.Article article, Long rssFeedId);
 	public List<Article> getArticlesAvaialbleForAnalysis();
 	public Article setCatgoriesForArticle(Article article, Map<String, Double> t);
+	public List<Article> getArticlesByCategories(List<Category> categories);
 }

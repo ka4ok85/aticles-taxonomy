431d311a5a0cf3104f22f0988b326a60b116c2e9
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-02-19 16:24:11 -0500

    added check for category list size

--- src/main/java/com/example/aticlestaxonomy/rest/v1/ArticleController.java
+++ src/main/java/com/example/aticlestaxonomy/rest/v1/ArticleController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.aticlestaxonomy.dto.Article;
 import com.example.aticlestaxonomy.dto.Category;
 import com.example.aticlestaxonomy.dto.ErrorInfo;
 import com.example.aticlestaxonomy.services.ArticleService;
@@ -29,13 +29,21 @@ public class ArticleController {
 	@Autowired
 	private ArticleService articleService;
 
+	@Value("${endpoint.articles-search.categories.max}")
+	private int categoriesMax;
+
 	@RequestMapping(value = "/articles/search", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
 	public ResponseEntity<?> getArticles(@RequestBody List<Category> categories) {
-
-		List<Article> articles = new ArrayList<Article>();
 		if (categories.size() < 1) {
 			ErrorInfo errorInfo = new ErrorInfo("Bad Request", HttpServletResponse.SC_BAD_REQUEST,
 					"No categories were specified in Request", this.getClass().getName());
+
+			return new ResponseEntity<ErrorInfo>(errorInfo, HttpStatus.BAD_REQUEST);
+		}
+
+		if (categories.size() > categoriesMax) {
+			ErrorInfo errorInfo = new ErrorInfo("Bad Request", HttpServletResponse.SC_BAD_REQUEST,
+					"More than " + categoriesMax + " categories were specified in Request", this.getClass().getName());
 
 			return new ResponseEntity<ErrorInfo>(errorInfo, HttpStatus.BAD_REQUEST);
 		}
@@ -49,30 +57,10 @@ public class ArticleController {
 				return new ResponseEntity<ErrorInfo>(errorInfo, HttpStatus.BAD_REQUEST);
 			} else {
 				categoriesList.add(categoryService.findByCategory(category.getName()));
-				System.out.println(category.getName());
 			}
 		}
 
 		return new ResponseEntity<List<com.example.aticlestaxonomy.entities.Article>>(articleService.getArticlesByCategories(categoriesList), HttpStatus.OK);
-		/*
-		Long storeId = userDetailsService.getStoreId();
-		Page<Product> products;
-		if (isAlreadyInStore == null || isAlreadyInStore == true) {
-			products = productService.getProductsByStoreId(pageable, storeId);
-		} else {
-			products = productService.getProductsNotInStore(pageable, storeId);
-		}
-
-    	try {    	
-    		globalProductService.addProductIntoLocation(productLocationChangeWrapper.getProductId(), storeId, productLocationChangeWrapper.getLocationId(), productLocationChangeWrapper.getQuantity());
-    		return true;
-		} catch (Exception e) {
-			ErrorInfo errorInfo = new ErrorInfo();
-			errorInfo.setMessage(e.getMessage());
-			return new ResponseEntity<String>(errorInfo.getMessage(), HttpStatus.BAD_REQUEST);
-		}    
-*/
-		//return new ResponseEntity<List<Article>>(articles, HttpStatus.OK);
 	}
 
 }

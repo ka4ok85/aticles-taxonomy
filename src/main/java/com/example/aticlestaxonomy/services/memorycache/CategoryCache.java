ea68836bc20222b7e9ecb0413462118f25f89bc0
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-02-22 12:23:29 -0500

    added findByCategory() to category memory cache

--- src/main/java/com/example/aticlestaxonomy/services/memorycache/CategoryCache.java
+++ src/main/java/com/example/aticlestaxonomy/services/memorycache/CategoryCache.java
@@ -11,18 +11,27 @@ import com.example.aticlestaxonomy.repositories.CategoryRepository;
 
 public class CategoryCache {
 	private static Map<Long, String> categories = new HashMap<Long, String>();
+	private static Map<String, Long> ids = new HashMap<String, Long>();
 
 	@Autowired
 	private CategoryRepository categoryRepository;
 
 	@PostConstruct
 	public void init() {
-		categoryRepository.findAll().forEach(
-				category -> categories.put(category.getId(), category.getCategory())
+		categoryRepository.findAll()
+				.forEach(category -> {
+					categories.put(category.getId(), category.getCategory());
+					ids.put(category.getCategory(), category.getId());
+				}
+
 		);
 	}
 
 	public static String findById(Long id) {
 		return categories.get(id);
+	}
+
+	public static Long findByCategory(String category) {
+		return ids.get(category);
 	}
 }

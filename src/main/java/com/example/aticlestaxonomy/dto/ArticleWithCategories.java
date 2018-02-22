b8190156f4128d1a579e869c0fded43608144b62
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-02-22 11:53:56 -0500

    added constructor to DTO

--- src/main/java/com/example/aticlestaxonomy/dto/ArticleWithCategories.java
+++ src/main/java/com/example/aticlestaxonomy/dto/ArticleWithCategories.java
@@ -1,14 +1,33 @@
 package com.example.aticlestaxonomy.dto;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
+
+import com.example.aticlestaxonomy.entities.Article;
+import com.example.aticlestaxonomy.services.memorycache.CategoryCache;
 
 public class ArticleWithCategories {
 
 	private String url;
 	private String title;
 	private LocalDateTime createdDatetime;
-	private List<CategoryWithWeight> categories;
+	private List<CategoryWithWeight> categories = new ArrayList<CategoryWithWeight>();
+
+	public ArticleWithCategories(Article article) {
+		setUrl(article.getUrl());
+		setTitle(article.getTitle());
+		setCreatedDatetime(article.getCreatedDatetime());
+		
+		article.getArticleJoinCategorySet().stream().forEach(articleJoinCategory -> 
+			{
+				CategoryWithWeight category = new CategoryWithWeight();
+				category.setWeight(articleJoinCategory.getWeight());
+				category.setCategory(CategoryCache.findById(articleJoinCategory.getCategoryId()));
+				categories.add(category);
+			}
+		);
+	}
 
 	public String getUrl() {
 		return url;

a5ba9332bf51fd3081443da2a1fa9a39864edf31
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-02-23 12:13:18 -0500

    use String instead of category entity

--- src/main/java/com/example/aticlestaxonomy/services/ArticleService.java
+++ src/main/java/com/example/aticlestaxonomy/services/ArticleService.java
@@ -3,7 +3,6 @@ package com.example.aticlestaxonomy.services;
 import java.util.List;
 import java.util.Map;
 
-import com.example.aticlestaxonomy.entities.Category;
 import com.example.aticlestaxonomy.dto.ArticleWithCategories;
 import com.example.aticlestaxonomy.entities.Article;
 
@@ -11,6 +10,6 @@ public interface ArticleService {
 	public com.example.aticlestaxonomy.entities.Article addArticle(com.example.aticlestaxonomy.dto.Article article, Long rssFeedId);
 	public List<Article> getArticlesAvaialbleForAnalysis();
 	public Article setCatgoriesForArticle(Article article, Map<String, Double> t);
-	public List<Article> getArticlesByCategories(List<Category> categories);
-	public List<ArticleWithCategories> getArticlesWithCategoriesByCategories(List<Category> categories);
+	public List<Article> getArticlesByCategories(List<String> categories);
+	public List<ArticleWithCategories> getArticlesWithCategoriesByCategories(List<String> categories);
 }

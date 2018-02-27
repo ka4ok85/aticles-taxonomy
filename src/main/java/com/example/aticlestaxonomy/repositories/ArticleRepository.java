c491ae1dd454245e7e6d76054acf4fc3d9855dc7
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-02-27 17:33:40 -0500

    updated findByCategoryAndRssFeed query to accept list of rssFeeds

--- src/main/java/com/example/aticlestaxonomy/repositories/ArticleRepository.java
+++ src/main/java/com/example/aticlestaxonomy/repositories/ArticleRepository.java
@@ -16,6 +16,6 @@ public interface ArticleRepository extends CrudRepository<Article, Long> {
 	@Query(value = "SELECT articles.* FROM articles, article_join_category, categories WHERE `is_category_set`='1' AND article_join_category.article_id=articles.id AND article_join_category.category_id=categories.id AND categories.category in :categories LIMIT :limit", nativeQuery = true)
 	List<Article> findByCategory(@Param("categories") List<String> categories, @Param("limit") int limit);
 
-	@Query(value = "SELECT articles.* FROM articles, article_join_category, categories WHERE `is_category_set`='1' AND rss_feed_id = :rssFeedId and article_join_category.article_id=articles.id AND article_join_category.category_id=categories.id AND categories.category in :categories LIMIT :limit", nativeQuery = true)
-	List<Article> findByCategoryAndRssFeed(@Param("categories") List<String> categories, @Param("rssFeedId") int rssFeedId, @Param("limit") int limit);
+	@Query(value = "SELECT articles.* FROM articles, article_join_category, categories WHERE `is_category_set`='1' AND rss_feed_id IN :rssFeedList and article_join_category.article_id=articles.id AND article_join_category.category_id=categories.id AND categories.category in :categories LIMIT :limit", nativeQuery = true)
+	List<Article> findByCategoryAndRssFeed(@Param("categories") List<String> categories, @Param("rssFeedList") List<Integer> rssFeedList, @Param("limit") int limit);
 }

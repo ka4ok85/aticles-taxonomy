c1d72ab821f34889077da3321c224dab62d64f41
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2018-02-26 19:43:17 -0500

    added findByCategoryAndRssFeed query

--- src/main/java/com/example/aticlestaxonomy/repositories/ArticleRepository.java
+++ src/main/java/com/example/aticlestaxonomy/repositories/ArticleRepository.java
@@ -15,4 +15,7 @@ public interface ArticleRepository extends CrudRepository<Article, Long> {
 
 	@Query(value = "SELECT articles.* FROM articles, article_join_category, categories WHERE `is_category_set`='1' AND article_join_category.article_id=articles.id AND article_join_category.category_id=categories.id AND categories.category in :categories LIMIT :limit", nativeQuery = true)
 	List<Article> findByCategory(@Param("categories") List<String> categories, @Param("limit") int limit);
+
+	@Query(value = "SELECT articles.* FROM articles, article_join_category, categories WHERE `is_category_set`='1' AND rss_feed_id = :rssFeedId and article_join_category.article_id=articles.id AND article_join_category.category_id=categories.id AND categories.category in :categories LIMIT :limit", nativeQuery = true)
+	List<Article> findByCategoryAndRssFeed(@Param("categories") List<String> categories, @Param("rssFeedId") int rssFeedId, @Param("limit") int limit);
 }
